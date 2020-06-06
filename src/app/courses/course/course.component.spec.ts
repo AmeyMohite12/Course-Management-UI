bf56d745426c1c24ea5c375fbaa3bf224ef74e4a
Ameya <ameymohite99@gmail.com>
2020-06-06 14:12:49 +0530

    Testing @24%

--- src/app/courses/course/course.component.spec.ts
+++ src/app/courses/course/course.component.spec.ts
@@ -1,16 +1,80 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from "@angular/core/testing";
 
-import { CourseComponent } from './course.component';
+import { CourseComponent } from "./course.component";
 
-describe('CourseComponent', () => {
+import { CourseService } from "../../shared/course.service";
+
+import { ToastrService } from "ngx-toastr";
+import { ReactiveFormsModule, FormsModule, NgForm } from "@angular/forms";
+
+import { of } from "rxjs";
+import { Course } from "src/app/shared/course.model";
+
+const CourseInfo = {
+  id: 1,
+  description: "h",
+  creator: "String",
+  skill: "String",
+  prerequisite: "String",
+  lastupdated: "String",
+};
+
+class MockCourseService {
+  updateCourse(form: Course) {
+    return of([]);
+  }
+
+  postCourse(form: Course) {
+    return of([]);
+  }
+  refreshList() {}
+}
+
+class MockToastrService {
+  info() {}
+  success() {}
+}
+const testForm = <NgForm>{
+  resetForm: () => null,
+
+  value: {
+    id: 1,
+    description: "h",
+    creator: "String",
+    skill: "String",
+    prerequisite: "String",
+    lastupdated: "String",
+  },
+};
+
+const testFormNull = <NgForm>{
+  resetForm: () => null,
+
+  value: {
+    id: null,
+    description: "h",
+    creator: "String",
+    skill: "String",
+    prerequisite: "String",
+    lastupdated: null,
+  },
+};
+fdescribe("CourseComponent", () => {
   let component: CourseComponent;
   let fixture: ComponentFixture<CourseComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ CourseComponent ]
-    })
-    .compileComponents();
+      declarations: [CourseComponent],
+      imports: [FormsModule, ReactiveFormsModule],
+      providers: [
+        { provide: CourseService, useClass: MockCourseService },
+        {
+          provide: ToastrService,
+          useClass: MockToastrService,
+        },
+      ],
+    }).compileComponents();
   }));
 
   beforeEach(() => {
@@ -19,7 +83,19 @@ describe('CourseComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it("should create", () => {
     expect(component).toBeTruthy();
+  });
+
+  it("should create resetForm If path", () => {
+    expect(component.resetForm(testForm));
+  });
+
+  it("call onSubmit() with non-null value", () => {
+    component.onSubmit(testForm);
+  });
+
+  it("call onSubmit() with null value", () => {
+    component.onSubmit(testFormNull);
   });
 });
--- src/app/courses/courses-list/courses-list.component.spec.ts
+++ src/app/courses/courses-list/courses-list.component.spec.ts
@@ -1,16 +1,46 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from "@angular/core/testing";
 
-import { CoursesListComponent } from './courses-list.component';
+import { CoursesListComponent } from "./courses-list.component";
+import { CourseService } from "../../shared/course.service";
 
-describe('CoursesListComponent', () => {
+import { ToastrService } from "ngx-toastr";
+import { of } from "rxjs";
+import { Course } from "src/app/shared/course.model";
+class MockCourseService {
+  refreshList() {}
+  deleteCourse(id: number) {
+    return of([]);
+  }
+  formData: any;
+}
+const CourseInfo = {
+  id: 1,
+  description: "h",
+  creator: "String",
+  skill: "String",
+  prerequisite: "String",
+  lastupdated: "String",
+};
+class MockToastrService {
+  info() {}
+  success() {}
+  warning() {}
+}
+fdescribe("CoursesListComponent", () => {
   let component: CoursesListComponent;
   let fixture: ComponentFixture<CoursesListComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ CoursesListComponent ]
-    })
-    .compileComponents();
+      declarations: [CoursesListComponent],
+      providers: [
+        { provide: CourseService, useClass: MockCourseService },
+        {
+          provide: ToastrService,
+          useClass: MockToastrService,
+        },
+      ],
+    }).compileComponents();
   }));
 
   beforeEach(() => {
@@ -19,7 +49,14 @@ describe('CoursesListComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it("should create", () => {
     expect(component).toBeTruthy();
+  });
+
+  it("Call populate function ", () => {
+    expect(component.populateForm(CourseInfo));
+  });
+  it("Call deleteFunction ", () => {
+    expect(component.deleteRecord(1));
   });
 });
